c0edf5acd97983afb2260703e6a982ab923bdcc7
Blue-Sky-r <robert.puskajler@yahoo.ca>
2021-01-17 00:16:23 +0100

    work in progress ...

--- bin/nas-dir.py
+++ bin/nas-dir.py
@@ -2,22 +2,22 @@
 
 import sys
 
-__VERSION__ = "2020.12.30"
+__VERSION__ = "2020.01.16"
 
-__ABOUT__  = "= NAS directory browser in Json format = version %s =" % (__VERSION__)
+__ABOUT__  = "= NAS directory browser in Json format {id: string, title: string}= version %s =" % (__VERSION__)
 
 __USAGE__  = """
 %(about)s
 
-usage: %(exe)s NasRoot RelDir PageNum PageSize
+usage: %(exe)s NasRoot RelDir IdxFrom IdxTo
 
 NasRoot  ... NAS root directory
 RelDir   ... relative dir to NAS root dir
-PageNum  ... page to display
-PageSize ... page size
+IdxFrom  ... index from
+IdxTo    ... index to
 """ % { 'about': __ABOUT__, 'exe': sys.argv[0] }
 
-VERBOSE = 0
+VERBOSE = 5
 
 import os, json, time, re
 
@@ -42,6 +42,7 @@ class Nas:
     def __init__(self, root):
         """ NAs root dir """
         self.root = root
+        self.nas = 'NAS:'
 
     def is_nas_mounted():
         """ check if NAS dir is auto-mounted via autofs """
@@ -60,7 +61,7 @@ class Nas:
         """ which entries to show in dir listing - subdirs + media files """
         return entry.is_dir() or self.is_media_file(entry.name)
 
-    def get_full_path(self, relpath):
+    def get_full_pathx(self, relpath):
         """ combine NAS root and relpath (while removing dir format)"""
         # regex to match formatted dir entry
         regex = '(' + self.dirFormat % '(.+)' + ')'
@@ -73,8 +74,22 @@ class Nas:
         # result
         return fullpath
 
-    def get_dir_page(self, relpath, pagenum, sortfnc, pagesize):
-        """ list directory entries for page pagenum and pagesize """
+    def get_full_path(self, relpath):
+        """ combine NAS root and relpath (while removing dir format)"""
+        path = os.path.join(self.root, relpath)
+        msg(4, 'DBG4: get_full_path(%s) -> "%s"' %(relpath, path))
+        return path
+
+    def format_item(self, entry):
+        """ format/build item from entry / format directories """
+        return {
+            "id":    self.get_full_path(entry.name),
+            "title": self.dirFormat % entry.name if entry.is_dir() else entry.name
+        }
+
+    def get_dir_fromto(self, relpath, idxfrom, idxto, sortfnc):
+        """ list directory entries for index from..to """
+        msg(5, 'DBG5: get_dir_fromto(relpath:%s, idxfrom:%d, idxto:%d)' % (relpath, idxfrom, idxto))
         #full = [ d for d in os.scandir(os.path.join(self.root, path)) if d.is_dir() or self.is_media_file(d.name) ]
         full = []
         with os.scandir(self.get_full_path(relpath)) as sd:
@@ -83,19 +98,19 @@ class Nas:
                     full.append(e)
         msg(5, 'DBG5: full-unsorted-list: %s' % full)
         # sort and splice
-        page = sorted(full, key=sortfnc)[pagenum:(pagenum+1)*pagesize]
+        page = sorted(full, key=sortfnc)[idxfrom:idxto+1]
         msg(4, 'DBG4: sorted-page: %s' % page)
         # format
-        return [ self.dirFormat % e.name if e.is_dir() else e.name for e in page ]
+        return [ self.format_item(e) for e in page ]
 
-    def get_dir_page_alpha(self, relpath, pagenum=0, pagesize=10):
-        """ list dir entries alphabetically for pagenum and pagesize """
-        return self.get_dir_page(relpath, pagenum, lambda entry: entry.name, pagesize)
+    def get_dir_alpha(self, relpath, idxfrom, idxto):
+        """ list dir entries alphabetically for idx range """
+        return self.get_dir_fromto(relpath, idxfrom, idxto, lambda entry: entry.name)
 
-    def get_dir_page_new(self, relpath, pagenum=0, pagesize=10):
-        """ list dir entries by date added (the newest first) for pagenum and pagesize """
+    def get_dir_new(self, relpath, idxfrom, idxto):
+        """ list dir entries by date added (the newest first) for idx range """
         now = time.time()
-        return self.get_dir_page(relpath, pagenum, lambda entry: now - entry.stat().st_mtime, pagesize)
+        return self.get_dir_fromto(relpath, idxfrom, idxto, lambda entry: now - entry.stat().st_mtime)
 
 
 if __name__ == '__main__':
@@ -104,13 +119,13 @@ if __name__ == '__main__':
 
     nasRoot  = sys.argv[1]
     relDir   = sys.argv[2]
-    pageNum  = int(sys.argv[3])
-    pageSize = int(sys.argv[4])
+    idxFrom  = int(sys.argv[3])
+    idxTo    = int(sys.argv[4])
 
-    msg(3, "DBG3: nasRoot(%s) relDir(%s) pageNum(%d) pageSize(%d)" % (nasRoot, relDir, pageNum, pageSize))
+    msg(3, "DBG3: nasRoot(%s) relDir(%s) idxFrom(%d) idxTo(%d)" % (nasRoot, relDir, idxFrom, idxTo))
 
     ns = Nas(nasRoot)
-    ls = ns.get_dir_page_new(relDir, pageNum, pageSize)
+    ls = ns.get_dir_alpha(relDir, idxFrom, idxTo)
 
     msg(3, 'DBG3: ls: %s' %ls)
 
